2f7f62a8c609e17e9b0d33e01ce1d7eef9b80f4e
taron <ghlijyantaron@gmail.com>
2023-01-29 13:35:11 -0600

    first push

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { Redirect, Route } from 'react-router-dom';
 import {
   IonApp,
+  IonFab,
+  IonFabButton,
   IonIcon,
   IonLabel,
   IonRouterOutlet,
@@ -10,10 +12,9 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import * as icons from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
-import Tab3 from './pages/Tab3';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -33,22 +34,26 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import { Storage } from '@ionic/storage';
+import { useState } from 'react';
+
 
 setupIonicReact();
 
-const App: React.FC = () => (
-  <IonApp>
+
+
+const App: React.FC = () => {
+
+  return <IonApp>
     <IonReactRouter>
       <IonTabs>
         <IonRouterOutlet>
           <Route exact path="/tab1">
-            <Tab1 />
+         <Tab1/> 
+            
           </Route>
           <Route exact path="/tab2">
             <Tab2 />
-          </Route>
-          <Route path="/tab3">
-            <Tab3 />
           </Route>
           <Route exact path="/">
             <Redirect to="/tab1" />
@@ -56,21 +61,19 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
-            <IonLabel>Tab 1</IonLabel>
+            <IonIcon icon={icons.timeOutline} />
+            <IonLabel>Schedule</IonLabel>
+            
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
+            <IonIcon icon={icons.listOutline} />
             <IonLabel>Tab 2</IonLabel>
-          </IonTabButton>
-          <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
-            <IonLabel>Tab 3</IonLabel>
           </IonTabButton>
         </IonTabBar>
       </IonTabs>
     </IonReactRouter>
+    
   </IonApp>
-);
+};
 
 export default App;
--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,24 +1,193 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonButtons, IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonDatetime, IonFab, IonFabButton, IonHeader, IonIcon, IonInput, IonItem, IonLabel, IonList, IonModal, IonPage, IonReorder, IonReorderGroup, IonSelect, IonSelectOption, IonTitle, IonToolbar, ItemReorderEventDetail } from '@ionic/react';
 import './Tab1.css';
+import * as icons from 'ionicons/icons';
+import { OverlayEventDetail } from '@ionic/react/dist/types/components/react-component-lib/interfaces';
+import { useRef, useState } from 'react';
+import { format, parseISO } from 'date-fns';
+
 
 const Tab1: React.FC = () => {
+  const weekday = ["sunday", "monday", "wuesday", "wednesday", "thursday", "friday", "saturday"];
+
+  const d = new Date();
+  let day = weekday[d.getDay()];
+
+  /*
+    {
+      name:"",
+      description: "",
+      startTime: "",
+      endTime: "",
+      text: ""
+    }
+  */
+
+  function handleReorder(event: CustomEvent<ItemReorderEventDetail>) {
+    console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
+    event.detail.complete();
+  }
+
+
+
+  const modal = useRef<HTMLIonModalElement>(null);
+  const [schedule, setSchedule] = useState<any[][]>([
+    [],
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+  ]);
+  const [name, setTaskName] = useState<string | number | undefined | null>("");
+  const [description, setTaskDescription] = useState<string | number | undefined | null>("");
+  const [startTime, setStartTime] = useState<string | number | undefined | null>(0);
+  const [endTime, setEndTime] = useState<string | number | undefined | null>(0);
+
+  function confirm() {
+    modal.current?.dismiss({
+      taskName: name,
+      taskDescription: description,
+      taskStartTime: startTime,
+      taskEndTime: endTime
+    }, 'confirm');
+  }
+
+  function onWillDismiss(ev: CustomEvent<OverlayEventDetail>) {
+    console.log(ev.detail.data);
+
+    if (ev.detail.role === 'confirm') {
+      let newS: object[][];
+      if (schedule[0]) {
+        newS = [...schedule];
+      } else {
+        newS = [
+          [],
+          [],
+          [],
+          [],
+          [],
+          [],
+          []
+        ]
+      }
+
+      newS[d.getDay()].push({
+        name: ev.detail.data.taskName,
+        description: ev.detail.data.taskDescription,
+        startTime: ev.detail.data.taskStartTime,
+        endTime: ev.detail.data.taskEndTime
+      });
+      setSchedule(newS);
+
+    }
+  }
+
   return (
     <IonPage>
-      <IonHeader>
+      <IonHeader collapse="condense">
         <IonToolbar>
-          <IonTitle>Tab 1</IonTitle>
+          <IonTitle size="large">Schedule</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 1</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 1 page" />
+
+
+
+        <IonSelect value={day}>
+          <IonSelectOption value="monday">Monday</IonSelectOption>
+          <IonSelectOption value="tuesday">Tuesday</IonSelectOption>
+          <IonSelectOption value="wednesday">Wednesday</IonSelectOption>
+          <IonSelectOption value="thursday">Thursday</IonSelectOption>
+          <IonSelectOption value="friday">Friday</IonSelectOption>
+          <IonSelectOption value="saturday">Saturday</IonSelectOption>
+          <IonSelectOption value="sunday">Sunday</IonSelectOption>
+        </IonSelect>
+
+
+        <IonReorderGroup disabled={false} onIonItemReorder={handleReorder}>
+
+          {
+            schedule[0] && schedule[d.getDay()].map((elem, i) => {
+              if (elem) {
+                console.log(elem);
+
+                // return <IonCard key={i}></IonCard>
+                return <IonItem lines="none" key={i}>
+                  <IonCard>
+                  <IonCardHeader>
+                    <IonCardTitle></IonCardTitle>
+                    <IonCardSubtitle>{elem.name}</IonCardSubtitle>
+                  </IonCardHeader>
+
+                  <IonCardContent>
+                    aa
+                  </IonCardContent>
+                </IonCard>
+                  <IonReorder slot="end"></IonReorder>
+                </IonItem>
+              }
+
+            })
+          }
+
+
+
+        </IonReorderGroup>
+
+        <IonFab slot="fixed" vertical="bottom" horizontal="end" >
+          <IonFabButton id="open-modal">
+            <IonIcon icon={icons.add}></IonIcon>
+          </IonFabButton>
+        </IonFab>
+
+
+        <IonModal ref={modal} trigger="open-modal" onWillDismiss={(ev) => onWillDismiss(ev)}>
+          <IonHeader>
+            <IonToolbar>
+              <IonButtons slot="start">
+                <IonButton onClick={() => modal.current?.dismiss()}>Cancel</IonButton>
+              </IonButtons>
+              <IonTitle>Add Task</IonTitle>
+              <IonButtons slot="end">
+                <IonButton strong={true} disabled={false} onClick={() => confirm()}>
+                  Confirm
+                </IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding">
+            <IonItem>
+              <IonLabel position="stacked">Enter task name</IonLabel>
+              <IonInput type="text" placeholder="Task name" value={name} onIonChange={(e) => { setTaskName(e.target.value) }} />
+
+              <IonLabel position="stacked">Enter task description</IonLabel>
+              <IonInput type="text" placeholder="Task description" value={description} onIonChange={(e) => { setTaskDescription(e.target.value) }} />
+
+              <IonLabel position="stacked">Enter start time</IonLabel>
+              <IonDatetime presentation='time' hourCycle='h23' onIonChange={(e) => {
+                const dateFromIonDatetime = e.detail.value as string;
+                const hour = format(parseISO(dateFromIonDatetime), 'H');
+                const minute = format(parseISO(dateFromIonDatetime), 'm');
+                console.log(minute); // Jun 4, 2021
+                setStartTime(hour + ":" + minute)
+              }}></IonDatetime>
+
+              <IonLabel position="stacked">Enter end time</IonLabel>
+              <IonDatetime presentation='time' hourCycle='h23' onIonChange={(e) => {
+                const dateFromIonDatetime = e.detail.value as string;
+                const hour = format(parseISO(dateFromIonDatetime), 'H');
+                const minute = format(parseISO(dateFromIonDatetime), 'm');
+                console.log(minute); // Jun 4, 2021
+                setEndTime(hour + ":" + minute)
+              }}></IonDatetime>
+
+            </IonItem>
+          </IonContent>
+        </IonModal>
+
       </IonContent>
-    </IonPage>
+    </IonPage >
   );
 };
 
